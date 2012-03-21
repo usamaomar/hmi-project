44783569fcdf229f0e71873ea6d856e44a3b30e0
rgroothedde@gmail.com <rgroothedde@gmail.com@b01ea2cb-4f46-97f8-229e-eff2dfe18d1b>
2012-03-21 13:52:32 +0000

    source added

--- src/visualgtd/nl/VisualgtdActivity.java
+++ src/visualgtd/nl/VisualgtdActivity.java
@@ -1,13 +1,27 @@
 package visualgtd.nl;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 
 public class VisualgtdActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+    private TasksDbAdapter mDbHelper;
+	private Cursor mTaskCursor;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mDbHelper = new TasksDbAdapter(this);
+        mDbHelper.open();
+        fillData();
+        
     }
+
+	private void fillData() {
+		mTaskCursor = mDbHelper.fetchAllTasks();
+		startManagingCursor(mTaskCursor);
+	}
 }
