003cda30a367a7ca65fa4642d7bb37c575b13a88
rgroothedde@gmail.com <rgroothedde@gmail.com@b01ea2cb-4f46-97f8-229e-eff2dfe18d1b>
2012-03-27 09:44:15 +0000

    new project added

---
+++ visualtasks/src/visualtasks/com/VisualtasksActivity.java
@@ -0,0 +1,13 @@
+package visualtasks.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VisualtasksActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
