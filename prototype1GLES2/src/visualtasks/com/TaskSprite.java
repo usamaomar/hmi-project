978e7d9281a6de0b1c4b90546eed9460a933ab16
rgroothedde@gmail.com <rgroothedde@gmail.com@b01ea2cb-4f46-97f8-229e-eff2dfe18d1b>
2012-04-10 11:39:47 +0000

    Eerste prototype rc

--- prototype1GLES2/src/visualtasks/com/TaskSprite.java
+++ prototype1GLES2/src/visualtasks/com/TaskSprite.java
@@ -17,18 +17,18 @@ import android.os.Bundle;
 
 public class TaskSprite extends TextSprite implements IClickDetectorListener,IPinchZoomDetectorListener, IHoldDetectorListener, IScrollDetectorListener{
 	
-	private static final int TRIGGER_HOLD_MIN_MILISECONDS = 300;
-	private static final float SCALE_MIN = 1f;
-	private static final float SCALE_MAX = 2f;
 	private final static float SCALE_FACTOR = 0.5f;
-	private HoldDetector mHoldDetector; 
+	private static final float SCALE_MAX = 2f;
+	private static final float SCALE_MIN = 1f;
+	private static final int TRIGGER_HOLD_MIN_MILISECONDS = 300;
+	private boolean isTouched; 
+	private Visualtasks mContext;
+	private HoldDetector mHoldDetector;
+
 	private PinchZoomDetector mPinchZoomDetector;
 	private ScrollDetector mScrollDetector;
-
-	private boolean isTouched;
-	private Visualtasks mContext;
-	private Task mTask;
 	private float mStartScaleX, mStartScaleY;
+	private Task mTask;
 	public TaskSprite(Visualtasks pContext, Task pTask, Font pFont,ITextureRegion pTextureRegion, VertexBufferObjectManager vBOM) {
 		super(pTask.getDescription(),pFont, pTextureRegion, vBOM);
 		this.mTask = pTask;
@@ -37,6 +37,15 @@ public class TaskSprite extends TextSprite implements IClickDetectorListener,IPi
 		// TODO Auto-generated constructor stub
 	}
 	
+	private float getScaleFromUrgency(Float urgency){
+		return (SCALE_MAX-urgency) * SCALE_FACTOR;
+	}
+		
+	private float getUrgencyFromScale(Float scale){
+		return SCALE_MAX-(scale/SCALE_FACTOR);
+	}
+	
+	
 	private void init(){
 		this.setScale(SCALE_FACTOR);
 		this.mHoldDetector = new HoldDetector(this);
@@ -44,124 +53,14 @@ public class TaskSprite extends TextSprite implements IClickDetectorListener,IPi
 		this.mScrollDetector = new ScrollDetector(this);
 		this.mHoldDetector.setTriggerHoldMinimumMilliseconds(TRIGGER_HOLD_MIN_MILISECONDS);
 	}
-		
-	@Override
-	protected void onManagedUpdate(float pSecondsElapsed) {
-		this.setPosition(mTask.getX(), mTask.getY());
-		this.setText(mTask.getDescription());
-			
-			if(!this.mPinchZoomDetector.isZooming()){
-				this.setScale(this.getScaleFromUrgency(mTask.getUrgency()));
-			}
-		
-		super.onManagedUpdate(pSecondsElapsed);
-		
-	}
-	
-	
-	@Override
-	public void onPinchZoom(PinchZoomDetector arg0, TouchEvent arg1, float pZoomFactor) {
-		// bound x and y
-		float scaleX = Math.max(Math.min(pZoomFactor * mStartScaleX, SCALE_MAX * SCALE_FACTOR),SCALE_MIN * SCALE_FACTOR);
-		float scaleY = Math.max(Math.min(pZoomFactor * mStartScaleY, SCALE_MAX * SCALE_FACTOR),SCALE_MIN * SCALE_FACTOR);
-		this.setScale(scaleX,  scaleY);
-				
-	
-	}
 	
 	
 	
-	@Override
-	public void onPinchZoomFinished(PinchZoomDetector arg0, TouchEvent arg1,float pZoomFactor) {
-		final float urgency = this.getUrgencyFromScale(this.getScaleX());
-		this.mTask.setUrgency(urgency);
-		mContext.sortTasks();
-		
-	}
-	
-	private float getUrgencyFromScale(Float scale){
-		return SCALE_MAX-(scale/SCALE_FACTOR);
-	}
-	
-	private float getScaleFromUrgency(Float urgency){
-		return (SCALE_MAX-urgency) * SCALE_FACTOR;
-	}
-	@Override
-	public void onPinchZoomStarted(PinchZoomDetector arg0, TouchEvent arg1) {
-		
-			this.mStartScaleX = this.getScaleX();
-			this.mStartScaleY = this.getScaleY();
-		
-	
-	}
-	
-	@Override
-	public void onClick(ClickDetector arg0, int arg1, float arg2, float arg3) {
-		
-			//this.setPosition(this.getX() + pDistanceX, this.getY() + pDistanceY);
-		
-	
-	}
-	@Override
-	public void onHold(HoldDetector arg0, long arg1, int arg2, float arg3,
-			float arg4) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void onHoldFinished(HoldDetector arg0, long arg1, int arg2,
-			float arg3, float arg4) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void onHoldStarted(HoldDetector arg0, int arg1, float arg2,
-			float arg3) {
-		
-			this.mHoldDetector.setEnabled(false);
-			final Bundle bundle = new Bundle();
-			bundle.putSerializable(Visualtasks.KEY_TASK, this.mTask);
-			this.mContext.runOnUiThread(new Runnable(){
-				@Override
-				public void run() {
-					TaskSprite.this.mContext.removeDialog(Visualtasks.DIALOG_CONTEXT_ID);
-					TaskSprite.this.mContext.showDialog(Visualtasks.DIALOG_CONTEXT_ID, bundle);
-					
-				}});
-			this.mHoldDetector.setEnabled(true);
-		
-	}
-	
-	
-	
-	@Override
-	public void onScroll(ScrollDetector pScrollDetector,  final int pPointerID, final float pDistanceX, final float pDistanceY) {
-		
-		mTask.setX(this.getX() + pDistanceX);
-		mTask.setY(this.getY() + pDistanceY);
-		
-	}
-	@Override
-	public void onScrollFinished(ScrollDetector pScrollDetector,  final int pPointerID, final float pDistanceX, final float pDistanceY) {
-		
-				mTask.setX(this.getX() + pDistanceX);
-				mTask.setY(this.getY() + pDistanceY);
-	
-		
-	}
-	@Override
-	public void onScrollStarted(ScrollDetector pScrollDetector,  final int pPointerID, final float pDistanceX, final float pDistanceY) {
-		
-				
-	}
-	
-	
-
 	@Override
 	public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float arg2, float arg3) {
 			switch(pSceneTouchEvent.getAction()){
 			case TouchEvent.ACTION_OUTSIDE:
-				break;
+				
 			case TouchEvent.ACTION_DOWN:
 			case TouchEvent.ACTION_MOVE:
 			default:
@@ -186,15 +85,124 @@ public class TaskSprite extends TextSprite implements IClickDetectorListener,IPi
 			
 			switch(pSceneTouchEvent.getAction()){
 			case TouchEvent.ACTION_DOWN:
-				this.isTouched = true;
+			case TouchEvent.ACTION_MOVE:
+				if(this.mTask.isSelected() != true){
+					this.mTask.setSelected(true);
+					mContext.reorderTasks();
+				}
+				
 				break;
 			case TouchEvent.ACTION_UP:
 			case TouchEvent.ACTION_CANCEL:
-				this.isTouched = false;
+				if(this.mTask.isSelected() != false){
+					this.mTask.setSelected(false);
+					mContext.reorderTasks();
+				}
 			}
 				
 		
 		return true;
+	}
+	
+	@Override
+	public void onClick(ClickDetector arg0, int arg1, float arg2, float arg3) {
+		
+			//this.setPosition(this.getX() + pDistanceX, this.getY() + pDistanceY);
+		
+	
+	}
+	
+	@Override
+	public void onHold(HoldDetector arg0, long arg1, int arg2, float arg3,
+			float arg4) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onHoldFinished(HoldDetector arg0, long arg1, int arg2,
+			float arg3, float arg4) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onHoldStarted(HoldDetector arg0, int arg1, float arg2,
+			float arg3) {
+		
+			this.mHoldDetector.setEnabled(false);
+			final Bundle bundle = new Bundle();
+			bundle.putSerializable(Visualtasks.KEY_TASK, this.mTask);
+			this.mContext.runOnUiThread(new Runnable(){
+				@Override
+				public void run() {
+					TaskSprite.this.mContext.removeDialog(Visualtasks.DIALOG_CONTEXT_ID);
+					TaskSprite.this.mContext.showDialog(Visualtasks.DIALOG_CONTEXT_ID, bundle);
+					
+				}});
+			this.mHoldDetector.setEnabled(true);
+		
+	}
+	@Override
+	protected void onManagedUpdate(float pSecondsElapsed) {
+		this.setPosition(mTask.getX(), mTask.getY());
+		this.setText(mTask.getDescription());
+			
+			if(!this.mPinchZoomDetector.isZooming()){
+				this.setScale(this.getScaleFromUrgency(mTask.getUrgency()));
+			}
+		
+		super.onManagedUpdate(pSecondsElapsed);
+		
+	}
+	@Override
+	public void onPinchZoom(PinchZoomDetector arg0, TouchEvent arg1, float pZoomFactor) {
+		// bound x and y
+		this.setScale(pZoomFactor * mStartScaleX,  pZoomFactor * mStartScaleY);
+				
+	
+	}
+	@Override
+	public void onPinchZoomFinished(PinchZoomDetector arg0, TouchEvent arg1,float pZoomFactor) {
+		final float scaleX = Math.max(Math.min(this.getScaleX(), SCALE_MAX * SCALE_FACTOR),SCALE_MIN * SCALE_FACTOR);
+		
+		final float urgency = this.getUrgencyFromScale(scaleX);
+		this.mTask.setUrgency(urgency);
+		mContext.reorderTasks();
+		
+	}
+	
+	
+	
+	@Override
+	public void onPinchZoomStarted(PinchZoomDetector arg0, TouchEvent arg1) {
+		
+			this.mStartScaleX = this.getScaleX();
+			this.mStartScaleY = this.getScaleY();
+		
+	
+	}
+	@Override
+	public void onScroll(ScrollDetector pScrollDetector,  final int pPointerID, final float pDistanceX, final float pDistanceY) {
+		
+		mTask.setX(this.getX() + pDistanceX);
+		mTask.setY(this.getY() + pDistanceY);
+		
+	}
+	@Override
+	public void onScrollFinished(ScrollDetector pScrollDetector,  final int pPointerID, final float pDistanceX, final float pDistanceY) {
+		
+		mTask.setX(this.getX() + pDistanceX);
+		mTask.setY(this.getY() + pDistanceY);
+	
+		
+	}
+	
+	
+
+	@Override
+	public void onScrollStarted(ScrollDetector pScrollDetector,  final int pPointerID, final float pDistanceX, final float pDistanceY) {
+		
+				
 	}
 
 }
