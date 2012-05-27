231e9385a505149c27acf630ff7d14becdcc140d
rgroothedde@gmail.com <rgroothedde@gmail.com@b01ea2cb-4f46-97f8-229e-eff2dfe18d1b>
2012-05-27 22:16:51 +0000

    TaskSprite added, textSprite deleted, Task deleted

--- prototype1GLES-r/src/data/TaskDbHandler.java
+++ prototype1GLES-r/src/data/TaskDbHandler.java
@@ -13,7 +13,7 @@ public class TaskDbHandler extends SQLiteOpenHelper {
 
 	 // All Static variables
     // Database Version
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
  
     // Database Name
     private static final String DATABASE_NAME = "visualtasks";
@@ -55,23 +55,24 @@ public class TaskDbHandler extends SQLiteOpenHelper {
         onCreate(db);
     }
     
-    public void addTask(Task task) {
+    public long addTask(String description, float x, float y, int status, float urgency) {
         SQLiteDatabase db = this.getWritableDatabase();
      
         ContentValues values = new ContentValues();
-        values.put(KEY_DESCRIPTION, task.getDescription()); // 
-        values.put(KEY_POSX, task.getX()); // 
-        values.put(KEY_POSY, task.getY());
-        values.put(KEY_STATUS, task.getStatus());
-        values.put(KEY_URGENCY, task.getUrgency());
+        values.put(KEY_DESCRIPTION, description); // 
+        values.put(KEY_POSX, x); // 
+        values.put(KEY_POSY, y);
+        values.put(KEY_STATUS, status);
+        values.put(KEY_URGENCY, urgency);
         // Inserting Row
         Long id = db.insert(TABLE_TASKS, null, values);
-        task.setID(id);
+       
         db.close(); // Closing database connection
+        return id;
     }
     
     // Getting single task
-	public Task getTask(Long id) {
+	public Cursor getTask(Long id) {
 	    SQLiteDatabase db = this.getReadableDatabase();
 	 
 	    Cursor cursor = db.query(TABLE_TASKS, new String[] { KEY_ID,
@@ -81,74 +82,64 @@ public class TaskDbHandler extends SQLiteOpenHelper {
 	        cursor.moveToFirst();
 	 
 	  
-	    return cursorToTask(cursor);
+	    return cursor;
 	}
 	
     // Getting All Tasks
-	 public List<Task> getAllTasks() {
-	    List<Task> taskList = new ArrayList<Task>();
+	 public Cursor getAllTasks() {
+	 
 	    // Select All Query
 	    String selectQuery = "SELECT  * FROM " + TABLE_TASKS;
 	 
 	    SQLiteDatabase db = this.getWritableDatabase();
 	    Cursor cursor = db.rawQuery(selectQuery, null);
 	 
-	    // looping through all rows and adding to list
-	    if (cursor.moveToFirst()) {
-	        do {
-	            Task task = cursorToTask(cursor);
-	            // Adding task to list
-	            taskList.add(task);
-	        } while (cursor.moveToNext());
-	    }
-	    cursor.close();
-	    // return task list
-	    return taskList;
+	    return cursor;
 	}
 	 
 	// Updating single contact
-	 public int updateTask(Task task) {
+	 public int updateTask(Long id, String description, float x, float y, int status, float urgency) {
 	     SQLiteDatabase db = this.getWritableDatabase();
 	  
 	    ContentValues values = new ContentValues();
-	    values.put(KEY_DESCRIPTION, task.getDescription()); // 
-        values.put(KEY_POSX, task.getX()); // 
-        values.put(KEY_POSY, task.getY());
-        values.put(KEY_STATUS, task.getStatus());
-        values.put(KEY_URGENCY, task.getUrgency());
+	    values.put(KEY_DESCRIPTION, description); // 
+        values.put(KEY_POSX, x); // 
+        values.put(KEY_POSY, y);
+        values.put(KEY_STATUS, status);
+        values.put(KEY_URGENCY, urgency);
 	  
 	     // updating row
 	     return db.update(TABLE_TASKS, values, KEY_ID + " = ?",
-	             new String[] { String.valueOf(task.getID()) });
+	             new String[] { String.valueOf(id) });
 	 }
 	 
-	 // Updating all tasks
-	 public void updateAllTasks(List<Task> tasks){
-		 for(Task task : tasks){
-			 this.updateTask(task);
-		 }
-	 }
+//	 // Updating all tasks
+//	 public void updateAllTasks(List<Task> tasks){
+//		 for(Task task : tasks){
+//			 this.updateTask(task);
+//		 }
+//	 }
 	 
 	 
 	// Deleting single contact
-	 public void deleteTask(Task task) {
+	 public void deleteTask(Long id) {
 	     SQLiteDatabase db = this.getWritableDatabase();
 	     db.delete(TABLE_TASKS, KEY_ID + " = ?",
-	             new String[] { String.valueOf(task.getID()) });
+	             new String[] { String.valueOf(id) });
 	     db.close();
 	 }
 	
-	private Task cursorToTask(Cursor cursor){
-		Task task = new Task(
-	    		cursor.getInt(cursor.getColumnIndexOrThrow(KEY_ID)),
-	    		cursor.getString(cursor.getColumnIndexOrThrow(KEY_DESCRIPTION)),
-	    		cursor.getFloat(cursor.getColumnIndexOrThrow(KEY_POSX)),
-	    		cursor.getFloat(cursor.getColumnIndexOrThrow(KEY_POSY)),
-	    		cursor.getFloat(cursor.getColumnIndexOrThrow(KEY_URGENCY)),
-	    		cursor.getInt(cursor.getColumnIndexOrThrow(KEY_STATUS))
-	    		);
-	            
-	    return task;
-	}
+//	private Task cursorToTask(Cursor cursor){
+//		Task task = new Task(
+//	    		cursor.getInt(cursor.getColumnIndexOrThrow(KEY_ID)),
+//	    		cursor.getString(cursor.getColumnIndexOrThrow(KEY_DESCRIPTION)),
+//	    		cursor.getFloat(cursor.getColumnIndexOrThrow(KEY_POSX)),
+//	    		cursor.getFloat(cursor.getColumnIndexOrThrow(KEY_POSY)),
+//	    		cursor.getFloat(cursor.getColumnIndexOrThrow(KEY_URGENCY)),
+//	    		cursor.getInt(cursor.getColumnIndexOrThrow(KEY_STATUS))
+//	    		);
+//	            
+//	    return task;
+//	}
 
 }
---
+++ prototype1GLES-r/src/visualtasks/com/TaskSprite.java
@@ -0,0 +1,192 @@
+package visualtasks.com;
+
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.AutoWrap;
+import org.andengine.entity.text.Text;
+import org.andengine.entity.text.TextOptions;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsConnectorManager;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.physics.box2d.util.constants.PhysicsConstants;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.HorizontalAlign;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class TaskSprite extends Sprite {
+
+	private  Font mFont;
+	private Text mText;
+	
+	private static final float BORDER = 6f;
+	private final static float SCALE_FACTOR = 0.1f;
+	private static final float SCALE_MAX = 5f;
+	private static final float SCALE_DEFAULT = 0.2f;
+	private static final float SCALE_MIN = 0.1f;
+	
+	private PhysicsWorld mPhysicsWorld;
+	private FixtureDef mFixtureDef;
+	 private PhysicsConnector pc;
+	private boolean deleted = false;
+	private boolean bodyNeedsUpdate = false;
+	private long id;
+	private Body body;
+	private Scene mScene;
+	private VertexBufferObjectManager mVBO;
+	public TaskSprite(long id, Scene pScene, FixtureDef fixtureDef, PhysicsWorld physicsWorld, Font pFont, ITextureRegion pTextureRegion,	VertexBufferObjectManager vBOM) {
+		super(120,120,pTextureRegion, vBOM);
+		mVBO =  vBOM;
+		this.id = id;
+		this.mPhysicsWorld = physicsWorld;
+		this.mFixtureDef = fixtureDef;
+		this.mScene = pScene;
+		this.mFont = pFont;
+		createBody();
+//		mScene.registerUpdateHandler(this);
+		
+		
+	}
+
+	public static final int STATUS_ACTIVE = 0;
+	public static final int STATUS_COMPLETED = STATUS_ACTIVE + 1;
+	public static final int STATUS_DELETED = STATUS_COMPLETED + 1;
+	
+	public void delete(){
+		deleted = true;
+	}
+	
+	@Override
+	public void setX(float pX) {
+		
+		if (this.getBody()  == null){
+			super.setX(pX);
+		} else{
+			this.getBody().setTransform(pX/ PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT, this.getY()/PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT, 0);
+		}
+	}
+	
+	private Body getBody(){
+//		PhysicsConnectorManager pcm = mPhysicsWorld.getPhysicsConnectorManager();
+//		return pcm.findBodyByShape(this);
+		return body;
+	}
+	@Override
+	public void setY(float pY) {
+		if (this.getBody() == null){
+			super.setY(pY);
+		} else{
+			this.getBody().setTransform(this.getX()/PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT,pY/ PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT, 0);
+		}
+	}
+	
+	@Override
+	public float getY() {
+		if(this.getBody() == null){
+			return super.getY();
+		} else{
+			return this.getBody().getPosition().y * PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
+		}
+	}
+	
+	@Override
+	public float getX() {
+		if(this.getBody() == null){
+			return super.getX();
+		} else{
+			return this.getBody().getPosition().x * PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
+		}
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setText(String text){
+		if (mText != null) this.detachChild(mText);
+		this.mText = new Text(0, 0, this.mFont, text,	new TextOptions(AutoWrap.WORDS, this.getWidth(), HorizontalAlign.CENTER),mVBO);
+		
+		this.attachChild(mText);
+		mText.setPosition(this.getWidth() / 2f - mText.getWidth() / 2f,	this.getHeight() / 2f - mText.getHeight() / 2f);
+	}
+	public String getText(){
+		return (mText == null? "" : mText.getText().toString());
+	}
+	
+	
+	@Override
+	public void setScale(float pScale) {
+		pScale = Math.max(Math.min(pScale, this.SCALE_MAX),this.SCALE_MIN);
+		super.setScale(pScale);
+		bodyNeedsUpdate = true;
+		
+	}
+	
+	@Override
+	public void setScale(float pScaleX, float pScaleY) {
+		// TODO Auto-generated method stub
+		pScaleX = Math.max(Math.min(pScaleX, this.SCALE_MAX),this.SCALE_MIN);
+		pScaleY = Math.max(Math.min(pScaleY, this.SCALE_MAX),this.SCALE_MIN);
+		super.setScale(pScaleX, pScaleY);
+		bodyNeedsUpdate = true;
+	}
+	
+	private void createBody(){
+		body = PhysicsFactory.createCircleBody(mPhysicsWorld, this, BodyType.DynamicBody, mFixtureDef, PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT);
+    	  pc = new PhysicsConnector(this, body, true, true, PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT);
+    	 mPhysicsWorld.registerPhysicsConnector(pc);
+	}
+	
+	private void removeBody(){
+		if(this.getBody() != null){
+			mPhysicsWorld.unregisterPhysicsConnector(pc);
+			mPhysicsWorld.destroyBody(this.getBody());
+						
+		}
+	}
+	
+	public int getStatus(){
+		if (deleted) return STATUS_DELETED;
+		else if (this.getY() > BORDER) return STATUS_ACTIVE;
+		else return STATUS_COMPLETED;
+	}
+	
+	@Override
+	protected void onManagedUpdate(float pSecondsElapsed) {
+		if (bodyNeedsUpdate){
+			this.removeBody();
+			this.createBody();
+			bodyNeedsUpdate = false;
+		}
+		switch(this.getStatus()){
+		case STATUS_DELETED:
+			
+			break;
+		case STATUS_ACTIVE:
+			this.setAlpha(1f);
+			break;
+		case STATUS_COMPLETED:
+			this.setAlpha(0.3f);
+			break;
+		}
+		
+		//check if new body needed
+		super.onManagedUpdate(pSecondsElapsed);
+	}
+
+	
+		
+	public  float getUrgency(){
+		return SCALE_MAX-(this.getScaleX()/SCALE_FACTOR);
+	}
+	
+	public void setUrgency(float urgency){
+		 this.setScale((SCALE_MAX-urgency) * SCALE_FACTOR);
+	}
+	
+}
